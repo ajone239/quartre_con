6e221a5b5285f2c29255cd390b1e45a15f603a0e
Austin Jones <ajones53.aj@gmail.com>
2024-01-12 17:46:44 -0500

    colored printing

--- src/board/piece.rs
+++ src/board/piece.rs
@@ -1,5 +1,7 @@
 use std::fmt::{Debug, Display};
 
+use colored::*;
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 pub enum Piece {
     Yellow,
@@ -9,8 +11,8 @@ pub enum Piece {
 impl Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Piece::Yellow => write!(f, "Y"),
-            Piece::Red => write!(f, "R"),
+            Piece::Yellow => write!(f, "{}", "Y".yellow()),
+            Piece::Red => write!(f, "{}", "R".red()),
         }
     }
 }
